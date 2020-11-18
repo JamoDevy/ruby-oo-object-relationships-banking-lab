a27425b2bcb7a388765afd9fa89cab10a72b1020
devin <devinlclarke@gmail.com>
2020-11-18 13:08:14 -0500

    commit tree 7e2977a8cf988b5907b80672541847f7afcfb43f

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+
+    end
+
+    def deposit(num)
+        self.balance += num
+    end
+
+    def withdrawal(amount)
+        self.balance -= amount
+      end
+
+    def display_balance
+        "Your balance is $#{balance}."
+
+    end
+
+    def valid?
+      @balance > 0 && @status == "open"
+        
+    end
+
+    def close_account
+        self.status = "closed"
+
+    end
+
 
 end
